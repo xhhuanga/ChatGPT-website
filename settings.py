c3a0bd7522848868ff4817e0319c1c1f9b8588cd
xhhuanga <34839214+xhhuanga@users.noreply.github.com>
2023-07-13 04:31:51 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,10 +3,10 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = "36fe0504771f44aa9650c33f41566754"
 
 # openAi 官方 api
-URL = "https://api.openai.com/v1/chat/completions"
+#URL = "https://api.openai.com/v1/chat/completions"
 
 # openAi 代理 api
-# URL = "https://open.aiproxy.xyz/v1/chat/completions"
+URL = "https://yunlin.openai.azure.com/openai/deployments/test-35-turbo/chat/completions?api-version=2023-03-15-preview"
